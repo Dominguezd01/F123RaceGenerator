9721629a17a90776414f4a33fe9c6711808142c7
Dominguezd01 <migueldominguezroman01@gmail.com>
2024-04-12 23:49:59 +0200

    Basically the whole project

---
+++ src/lib/data.js
@@ -0,0 +1,49 @@
+export const gps = [
+    "Saudi Arabia",
+    "Australia",
+    "Imola",
+    "Miami",
+    "Spain",
+    "Azerbaijan",
+    "Canada",
+    "Great Britain",
+    "Austria",
+    "France",
+    "Belgium",
+    "Netherlands",
+    "Italy",
+    "Singapore",
+    "Austin",
+    "Mexico",
+    "Brazil",
+    "Yas marina",
+    "Portimao",
+    "China",
+]
+
+export const f1Teams = [
+    "Mercedes-AMG Petronas",
+    "Red Bull Racing",
+    "Scuderia Ferrari",
+    "McLaren F1 Team",
+    "Alpine F1 Team",
+    "Scuderia AlphaTauri",
+    "Aston Martin Formula One Team",
+    "Alfa Romeo Racing",
+    "Williams Racing",
+    "Haas F1 Team",
+]
+
+export const teamDriver = [
+    { "Mercedes-AMG Petronas": ["Lewis Hamilton", "George Rusell"] },
+    { "Red Bull Racing": ["Max Verstappen", "Sergio Pérez"] },
+    { "Scuderia Ferrari": ["Charles Leclerc", "Carlos Sainz"] },
+    { "McLaren F1 Team": ["Lando Norris", "Oscar Piastri"] },
+    { "Aston Martin Formula One Team": ["Fernando Alonso", "Lance Stroll"] },
+    { "Alpine F1 Team": ["Esteban Ocon", "Pierre Gasly"] },
+    { "Scuderia AlphaTauri": ["Yuki Tsnuoda", "Daniel Ricciardo"] },
+    { "Alfa Romeo Racing": ["Valteri Bottas", "Guanyu Zhou"] },
+    { "Williams Racing": ["Alex Albon", "Logan Sargent"] },
+    { "Haas F1 Team": ["Kevin Magnusen", "Nico Hulkenberg"] },
+]
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<script>
+  import { onMount } from "svelte"
+  import { gps, f1Teams, teamDriver } from "../lib/data.js"
+  let gp, team, driver, performance
+
+  const performances = ["Realitic", "Balanced"]
+
+  const getRandomGP = async () => gps[getRandomIndex(gps)]
+
+  const getRandomPerformance = async () =>
+    performances[[getRandomIndex(performances)]]
+
+  const getRandomTeamDriver = async () => {
+    let team = f1Teams[getRandomIndex(f1Teams)]
+    let driver = teamDriver.find((td) => td[team])[team][
+      getRandomIndex(teamDriver.find((td) => td[team])[team])
+    ]
+    return { team, driver }
+  }
+  onMount(async () => {
+    await getRandomValues()
+  })
+
+  const getRandomValues = async () => {
+    gp = await getRandomGP()
+    performance = await getRandomPerformance()
+    let teamDriver = await getRandomTeamDriver()
+    team = teamDriver.team
+    driver = teamDriver.driver
+  }
+
+  const getRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length)
+  }
+</script>
+
+<main class="flex flex-col gap-16 items-center justify-center p-3 text-center">
+  <div
+    class="flex flex-row gap-16 items-center justify-center border-0 p-3 text-center h-[580px] w-[1856px]"
+  >
+    <div class="flex flex-col gap-5">
+      <h1 class="text-6xl font-bold">GP</h1>
+      {#if gp !== undefined}
+        <h3 class="text-4xl">
+          {gp}
+        </h3>
+        {#await import(`$lib/tracks/${gp}.avif`) then { default: src }}
+          <img {src} class="w-[600px]" alt="Track" />
+        {/await}
+      {/if}
+    </div>
+    <div class="flex flex-col gap-5">
+      <h1 class="text-6xl font-bold">Team</h1>
+      {#if team !== undefined}
+        <h3 class="text-4xl">
+          {team}
+        </h3>
+        {#await import(`$lib/cars/${team}.png`) then { default: src }}
+          <img {src} alt="Driver" />
+        {/await}
+      {/if}
+    </div>
+    <div class="flex flex-col gap-5">
+      <h1 class="text-6xl font-bold">Driver</h1>
+      {#if driver !== undefined}
+        <h3 class="text-4xl">
+          {driver}
+        </h3>
+        {#await import(`$lib/drivers/${driver}.png`) then { default: src }}
+          <img {src} alt="Driver" />
+        {/await}
+      {/if}
+    </div>
+  </div>
+
+  <div class="flex items-center">
+    <button
+      on:click={getRandomValues}
+      class="border-2 p-3 rounded-md text-6xl text-center reload transition-all"
+    >
+      Reload
+    </button>
+  </div>
+</main>
+
+<style>
+  .reload:hover {
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(255, 255, 255, 0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(255, 255, 255, 0.75);
+    box-shadow: 10px 10px 5px 0px rgba(255, 255, 255, 0.75);
+  }
+</style>
